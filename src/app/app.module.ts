5d7db79162c5e295903e6bfb5d45f3ddd5f4fbc0
jinyongp <dev.jinyongp@gmail.com>
2023-07-16 03:00:05 +0900

    [Feat] PROM-39 OpenAPI 명세 추가 (w/ Swagger) (#11)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './controllers/app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from '@/config/orm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './modules/auth/auth.module';
-import { extraEnv } from './config/env';
-import { schema } from './config/validation';
+import { AuthModule } from '../modules/auth/auth.module';
+import { extraEnv } from '../config/env';
+import { schema } from '../config/validation';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -19,6 +20,14 @@ import { schema } from './config/validation';
       useFactory(config: ConfigService) {
         return typeormConfig(config);
       },
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        global: true,
+        secret: config.get('JWT_SECRET_KEY'),
+      }),
     }),
     AuthModule,
   ],
--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { EntryResponse } from '../dtos/entry.dto';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
   constructor(private readonly config: ConfigService) {}
 
   @Get()
-  async pong() {
+  @ApiOperation({ summary: 'Ping' })
+  @ApiOkResponse({ description: 'Pong', type: [EntryResponse] })
+  pong() {
     return {
-      message: 'Promise API',
+      message: 'pong',
       version: this.config.get('API_VERSION'),
       build: this.config.get('BUILD'),
       env: this.config.get('NODE_ENV'),
