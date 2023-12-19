4eaf7357e2d384282e828323d021e5c6c5fb5ebe
jinyongp <dev.jinyongp@gmail.com>
2023-12-19 21:44:57 +0900

    chore: serverless 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
+import type { Server } from 'http';
+import type { Handler } from 'aws-lambda';
+
+// @ts-expect-error - no types
+import { createServer, proxy } from '@vendia/serverless-express';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from '@/app/app.module';
-import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { WsAdapter } from '@nestjs/platform-ws';
 
-async function bootstrap() {
+let cachedServer: Server;
+const binaryMimeTypes: string[] = [];
+
+async function bootstrap(): Promise<Server> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = app.get(ConfigService);
-  const PORT = config.get('PORT') || 8080;
   const API_VERSION = config.get('API_VERSION');
 
-  app.useStaticAssets(join(__dirname, '..', 'assets'), { prefix: '/assets' });
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {
+    prefix: '/assets',
+  });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useWebSocketAdapter(new WsAdapter(app));
 
   const openApiConfig = new DocumentBuilder()
-    .setTitle(`Promise API`)
+    .setTitle('Promise API')
     .setVersion(API_VERSION)
     .addTag('App', 'Entry point of API')
     .addSecurity('bearer', { type: 'http', scheme: 'bearer' })
@@ -36,8 +45,12 @@ async function bootstrap() {
     },
   });
 
-  await app.listen(PORT, '0.0.0.0');
-  Logger.log(`Server running on ${await app.getUrl()}`, 'Bootstrap');
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return createServer(expressApp, undefined, binaryMimeTypes);
 }
 
-bootstrap();
+export const handler: Handler = async (event, context, callback) => {
+  cachedServer = cachedServer ?? (await bootstrap());
+  return proxy(cachedServer, event, context, 'PROMISE', callback).promise;
+};
