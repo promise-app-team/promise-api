b1f64ce0a1c1a42867e81dc314c9a655b5eb93a2
Park, Jinyong <dev.jinyongp@gmail.com>
2024-01-26 21:13:50 +0900

    PRMS-51 [Feat] AWS Lambda Migration (#29)
    * fix: lint 오류 수정
    
    * chore: serverless 설정 추가
    
    * chore: package 업데이트
    
    * feat: 로컬 환경에서 실행 가능하도록 수정
    
    * feat: serverless-express, date-fns 패키지 업데이트
    
    * fix: serverless 실행 문제 수정
    
    * feat: 파일 업로드 스펙 추가
    
    * feat: 시간 저장 오류 문제 수정
    
    * chore: assets 폴더 위치 변경
    
    * feat: serverless ecr 활용하도록 변경
    
    * ci: deploy github action script 추가

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
+import type { Handler } from 'aws-lambda';
+
+import createServer from '@codegenie/serverless-express';
 import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from '@/app/app.module';
 import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { AppModule } from '@/app/app.module';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const config = app.get(ConfigService);
-  const PORT = config.get('PORT') || 8080;
-  const API_VERSION = config.get('API_VERSION');
+const PORT = +(process.env.PORT || 3000);
+const API_VERSION = process.env.npm_package_version || '0.0.0';
 
-  app.useStaticAssets(join(__dirname, '..', 'assets'), { prefix: '/assets' });
+async function bootstrap<App extends NestExpressApplication>(): Promise<App> {
+  const app = await NestFactory.create<App>(AppModule);
+
+  app.useStaticAssets(join(__dirname, 'assets'), { prefix: '/assets' });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.enableCors();
 
   const openApiConfig = new DocumentBuilder()
-    .setTitle(`Promise API`)
+    .setTitle('Promise API')
     .setVersion(API_VERSION)
     .addTag('App', 'Entry point of API')
     .addSecurity('bearer', { type: 'http', scheme: 'bearer' })
@@ -27,17 +30,32 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, openApiConfig);
   SwaggerModule.setup('api', app, document, {
-    customfavIcon: `/assets/favicon.ico`,
     customSiteTitle: 'Promise API',
-    customCssUrl: `/assets/css/swagger.css`,
+    customfavIcon: '/assets/favicon.ico',
+    customCssUrl: '/assets/css/swagger.css',
     swaggerOptions: {
       docExpansion: 'none',
       persistAuthorization: true,
     },
   });
 
-  await app.listen(PORT, '0.0.0.0');
-  Logger.log(`Server running on ${await app.getUrl()}`, 'Bootstrap');
+  return app;
 }
 
-bootstrap();
+if (!process.env.IS_OFFLINE) {
+  (async () => {
+    const app = await bootstrap();
+    await app.listen(PORT, '0.0.0.0');
+    Logger.log(`Server running on ${await app.getUrl()}`, 'Bootstrap');
+  })();
+}
+
+let cached: Handler;
+export const handler: Handler = async (...args) => {
+  if (!cached) {
+    const app = await bootstrap();
+    await app.init();
+    cached = createServer({ app: app.getHttpAdapter().getInstance() });
+  }
+  return cached(...args);
+};
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { DeepPartial, Repository } from 'typeorm';
 import { Provider, UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { getUnixTime } from 'date-fns';
 
 @Injectable()
 export class UserService {
@@ -32,7 +31,7 @@ export class UserService {
   async login(user: UserEntity): Promise<UserEntity> {
     return this.userRepo.save(
       this.userRepo.merge(user, {
-        lastSignedAt: getUnixTime(new Date()),
+        lastSignedAt: new Date(),
       })
     );
   }
@@ -40,7 +39,7 @@ export class UserService {
   async delete(user: UserEntity, reason: string) {
     return this.userRepo.save(
       this.userRepo.merge(user, {
-        deletedAt: getUnixTime(new Date()),
+        deletedAt: new Date(),
         leaveReason: reason,
         providerId: null,
       })
