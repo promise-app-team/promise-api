e8aa49e72b7e39264800da7396a25068adc5d2e9
jinyongp <dev.jinyongp@gmail.com>
2024-01-29 14:42:57 +0000

    feat: middleware 로직 개선

--- src/modules/common/middlewares/trim.middleware.ts
+++ src/modules/common/middlewares/trim.middleware.ts
@@ -4,17 +4,18 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class TrimMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    if (req.body && typeof req.body === 'object') {
-      req.body = this.trimObjectStrings(req.body);
-    }
+    req.body = this.trimObjectStrings(req.body);
     next();
   }
 
   private trimObjectStrings(obj: any): any {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key) && typeof obj[key] === 'string') {
-        obj[key] = obj[key].trim();
-      } else if (typeof obj[key] === 'object') {
+    if (typeof obj === 'string') {
+      return obj.trim();
+    } else if (Array.isArray(obj)) {
+      return obj.map(this.trimObjectStrings);
+    } else if (typeof obj === 'object') {
+      for (const key in obj) {
+        if (!obj.hasOwnProperty(key)) continue;
         obj[key] = this.trimObjectStrings(obj[key]);
       }
     }
