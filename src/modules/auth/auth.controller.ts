57a9bb94053c7e8bae0e13e36f3cddbbab47a5d9
jinyongp <dev.jinyongp@gmail.com>
2024-04-01 16:03:50 +0900

    feat: refreshTokens 함수 권한 필요하도록 수정

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 import { HttpException } from '@/common/exceptions/http.exception';
 import { Post } from '@/customs/nest/decorators/http-api.decorator';
@@ -9,6 +9,7 @@ import { InputCreateUserDTO } from '@/modules/user/user.dto';
 import { UserService } from '@/modules/user/user.service';
 
 @ApiTags('Auth')
+@ApiBearerAuth()
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -19,10 +20,10 @@ export class AuthController {
   @Post('login', { description: '로그인 / 회원가입을 수행합니다.', exceptions: ['BAD_REQUEST'] })
   async login(@Body() input: InputCreateUserDTO): Promise<AuthTokenDTO> {
     const user = await this.userService.upsert(input);
-    return this.authService.authenticate(user).catch((error) => HttpException.throw(error));
+    return this.authService.authenticate(user).catch((error) => HttpException.throw(error, 'BAD_REQUEST'));
   }
 
-  @Post('refresh', { description: '인증 토큰을 갱신합니다.', exceptions: ['BAD_REQUEST'] })
+  @Post('refresh', { auth: true, description: '인증 토큰을 갱신합니다.', exceptions: ['BAD_REQUEST', 'NOT_FOUND'] })
   async refreshTokens(@Body() { refreshToken }: InputRefreshTokenDTO): Promise<AuthTokenDTO> {
     return this.authService.refresh(refreshToken).catch((error) => {
       switch (error) {
