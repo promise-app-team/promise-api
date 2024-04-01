133c10efcb24aa6e5720dbc8bacf5059db4e9437
jinyongp <dev.jinyongp@gmail.com>
2024-04-01 17:54:13 +0900

    test: upload.controller e2e 테스트 코드 추가

---
+++ src/tests/controllers/e2e-specs/upload.controller.e2e-spec.ts
@@ -0,0 +1,57 @@
+import path from 'node:path';
+
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+
+import { AppModule } from '@/app/app.module';
+import { FileUploadController } from '@/modules/upload/upload.controller';
+import { createUserBuilder } from '@/tests/fixtures/builder';
+import { createPrismaClient } from '@/tests/prisma';
+import { createHttpServer } from '@/tests/utils/http-server';
+
+const createUser = createUserBuilder(3e7);
+
+describe(FileUploadController, () => {
+  const prisma = createPrismaClient();
+  const http = createHttpServer<FileUploadController>({
+    uploadImageFile: '/upload/image',
+  });
+
+  let jwtService: JwtService;
+
+  const auth = {
+    user: {} as Awaited<ReturnType<typeof prisma.user.create>>,
+    token: '',
+  };
+
+  beforeAll(async () => {
+    auth.user = await prisma.user.create({ data: createUser() });
+  });
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    const app = module.createNestApplication();
+    http.prepare(await app.init());
+
+    jwtService = module.get(JwtService);
+    auth.token = jwtService.sign({ id: auth.user.id }, { expiresIn: '1h' });
+  });
+
+  describe(http.name.uploadImageFile, () => {
+    test('should return uploaded file url', async () => {
+      const res = await http.request.uploadImageFile.post
+        .auth(auth.token, { type: 'bearer' })
+        .attach('file', path.resolve(__dirname, '../../assets/promise-logo.png'))
+        .expect(201);
+
+      expect(/https:\/\/s3\..+\.amazonaws\.com\/.+\/.+/.test(res.body.url)).toBe(true);
+    });
+  });
+
+  afterAll(async () => {
+    await http.request.close();
+  });
+});
