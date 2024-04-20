6a5b06f550166db9b6b02b7a2cd4d80d51b5a927
jinyongp <dev.jinyongp@gmail.com>
2024-04-20 16:27:53 +0900

    chore: 자잘한 수정

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -26,7 +26,7 @@ export class AllExceptionsFilter implements ExceptionFilter {
       ? exception
       : HttpException.new('알 수 없는 오류가 발생했습니다.', 'INTERNAL_SERVER_ERROR', exception);
 
-    if (this.config.get('env') === 'test') {
+    if (this.config.get('is.test')) {
     } else if (typeof exception === 'string') {
       console.error('UnhandledException:', exception);
       console.trace();
