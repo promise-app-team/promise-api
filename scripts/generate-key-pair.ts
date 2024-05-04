7b91ccd74afb926bc1a29ae94f89016c4c9d536f
jinyongp <dev.jinyongp@gmail.com>
2024-05-05 01:13:24 +0900

    chore: 환경변수 자동 생성 스크립트 추가

--- scripts/generate-key-pair.ts
+++ scripts/generate-key-pair.ts
@@ -1,5 +1,7 @@
 import crypto from 'node:crypto';
 
+const isJson = process.argv.includes('--json');
+
 function generateKeyPair() {
   return crypto.generateKeyPairSync('ec', {
     namedCurve: 'P-256',
@@ -16,6 +18,15 @@ function generateKeyPair() {
 
 const { publicKey, privateKey } = generateKeyPair();
 
-console.log('Copy the key pair below and paste them in your .env file');
-console.log(`JWT_SIGN_KEY='${privateKey}'`.replace(/\r?\n|\r/g, '\\n').trim());
-console.log(`JWT_VERIFY_KEY='${publicKey}'`.replace(/\r?\n|\r/g, '\\n').trim());
+const keyPair = {
+  publicKey: publicKey.replace(/\r?\n|\r/g, '\\n'),
+  privateKey: privateKey.replace(/\r?\n|\r/g, '\\n'),
+};
+
+if (isJson) {
+  console.log(JSON.stringify(keyPair, null, 2));
+} else {
+  console.log('Copy the key pair below and paste them in your .env file\n');
+  console.log(`JWT_SIGN_KEY='${keyPair.privateKey}'`);
+  console.log(`JWT_VERIFY_KEY='${keyPair.publicKey}'`);
+}
