bc104acd87efa5b65965aa19b516374b7c74923c
jinyongp <dev.jinyongp@gmail.com>
2024-06-05 15:49:31 +0900

    fix: 필드 순서 조정

--- prisma/migrations/20240603013716_add_name_field_to_location_table/migration.sql
+++ prisma/migrations/20240603013716_add_name_field_to_location_table/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `pm_locations` ADD COLUMN `name` VARCHAR(20) NULL DEFAULT NULL;
+ALTER TABLE `pm_locations` ADD COLUMN `name` VARCHAR(20) NULL DEFAULT NULL AFTER `id`;
 
 -- RenameIndex
 ALTER TABLE `pm_mutation_logs` RENAME INDEX `pm_mutation_logs_user_id_fkey` TO `ix_user_id`;
